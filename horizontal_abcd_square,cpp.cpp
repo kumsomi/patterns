02dbde0d12cf838610775b45b40e7987826a72cc
Kum Somi <54243544+kumsomi@users.noreply.github.com>
2020-01-15 13:11:41 +0530

    abcd horizontally in a square pattern

---
+++ horizontal_abcd_square,cpp.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=5;
+	char c=65;
+	for(char i='A';i<='E';i++)
+	{
+		for(char j='A';j<='E';j++)
+		{
+			cout<<j;	
+		}cout<<endl;
+	}
+}
